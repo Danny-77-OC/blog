367e2c15f4195b29da56d07f9d1d61fc0d0b6f7c
daneric <kionga.eryk@gmail.com>
2020-12-05 16:13:33 +0100

    structure du projet

---
+++ view/inscription.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<form method="post" action="object.php" enctype="multipart/form-data">
+	
+	<label>pseudo</label><br>
+    <input type="text" name="pseudo"><br><br>
+    
+    <label>mot de passe</label><br>
+	<input type="text" name="mdp"><br><br>
+	
+
+
+	<input type="submit" value="Inscription">
+</form>
+
+
+
+</body>
+</html>
